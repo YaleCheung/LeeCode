20fb1e2fdecfce2c697f8eb840ffa1362a632253
zybjtu <zybjtu@gmail.com>
2015-01-07 20:07:46 +0800

    fix the binary search algorithm

--- chapter2/searchInRotatedSortedArray.cc
+++ chapter2/searchInRotatedSortedArray.cc
@@ -36,10 +36,9 @@ using namespace std;
 		     (i.e., i
 		     u are given a target value to search. If found in the array return its index, otherwise return -1. You may assume no duplicate exists in the array.
 		     */
-
 int Search(int array[], const int& size, const int& value) {
     int beg = 0;
-    int end = size - 1;
+    int end = size;
     while(beg != end) {
         int mid = (beg + end) / 2;
         if(value == array[mid])
@@ -50,19 +49,21 @@ int Search(int array[], const int& size, const int& value) {
             } else {
                 beg = mid + 1;
             }
-        } else {
-            if(value < array[end] && value >= array[end])
+        } else if(array[beg] > array[mid]) {
+            if(value <= array[end - 1] && value > array[mid])
                 beg = mid + 1;
             else
                 end = mid;
-        }
+        } else
+            ++ beg;
+
     }
     return -1;
 }
 
 
 int main(int argc, char* argv[]) {
-    int array[] = {4,5,6,7, 0, 1,2};
+    int array[] = {0,1,2};
     cout << Search(array, sizeof(array) / sizeof(int), 2);
     return 0;
 }
