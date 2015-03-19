08fa7223a27d38dfd6bdde217b98c10507d5c7e7
zybjtu <zybjtu@gmail.com>
2015-03-19 14:37:20 +0800

    rotate the array

--- chapter2/rotateArray.cc
+++ chapter2/rotateArray.cc
@@ -5,13 +5,29 @@ using std::cout;
 class Solution {
 public:
     void rotate(int nums[], int n, int k) {
-        if(n < 1)
-            return;
+        int pre_val = nums[0];
+        int post_val = nums[0];
+        int cur_pos = 0;
         int j = 0;
-        for(int i = n - k; i < n; ++ i) {
-            tmp = nums[j];
-            nums[j]= nums[i];
-            
+        for (int i = 0; i < n; ++ i) {
+            pre_val = post_val;
+            cur_pos = (cur_pos + k) % n;
+            post_val = nums[cur_pos];
+            nums[cur_pos] = pre_val;
+            if (cur_pos == j) {
+                cur_pos = ++ j;
+                post_val = nums[cur_pos];
+            }
         }
+        return;
     }
 };
+
+int main() {
+    int nums[] = {1,2};
+    Solution s;
+    s.rotate(nums, 2, 1);
+    for(auto i : nums)
+        cout << i << endl;
+    return 0;
+}
