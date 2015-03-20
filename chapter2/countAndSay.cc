ebac73a43b00846bf8959022acd5fde6a7e14194
zybjtu <zybjtu@gmail.com>
2015-03-20 09:06:50 +0800

    add array2String

---
+++ chapter2/countAndSay.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using std::endl;
+using std::cout;
+using string;
+
+#define ARRAYSIZE 10;
+string table[ARRAYSIZE] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+class Solution {
+public:
+    string countAndSay(int data) {
+	 sstream ss;
+	 ss << data;
+	 string val;
+	 ss >> val;
+
+    }
+};
