85fd09ed5fa17d625b736a0c814cc045050b2aac
zybjtu <zybjtu@gmail.com>
2015-03-21 08:00:20 +0800

    add minDepth.cc

--- chapter2/minDepth.cc
+++ chapter2/minDepth.cc
@@ -1,22 +1,24 @@
-//Programming: Given a binary tree, find its minimum depth.
-
 /**
- * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+ *@ Definition for binary tree
+ *@ struct TreeNode {
+ *@     int val;
+ *@     TreeNode *left;
+ *@     TreeNode *right;
+ *@     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *@ };
+ **/
 class Solution {
 public:
     int minDepth(TreeNode *root) {
-        // use recrusive method; 
-        if (root == NULL)
-            return -1;
+        if(NULL == root)
+            return 0;
         int left_depth = minDepth(root->left);
         int right_depth = minDepth(root->right);
-        return (left_depth > right_depth) ? right_depth + 1 : left_depth + 1;
+        if(0 == left_depth)
+            return right_depth + 1;
+        else if (0 == right_depth)
+            return left_depth + 1;
+        else
+            return left_depth < right_depth ? left_depth + 1 : right_depth + 1;
     }
 };
