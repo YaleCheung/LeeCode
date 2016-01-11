d434869bbc2979437707da9f28c8a09566cc8e60
YaleCheung <zybjtu@gmail.com>
2016-01-11 05:07:08 +0000

    add bulbSwitcher.cc

---
+++ chapter2/bulbSwitcher.cc
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  bulbSwitcher.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/11/16 05:05:42
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+class Solution {
+public:
+    int bulbSwitcher(int n) {
+        return int(sqrt(n));
+    }
+}
