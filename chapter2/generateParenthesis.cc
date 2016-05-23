865a4837ed71fa977baeb33b44a312b14bac60d2
YaleCheung <zybjtu@gmail.com>
2016-05-23 09:20:47 +0000

    add generateParenthesis.cc

---
+++ chapter2/generateParenthesis.cc
@@ -0,0 +1,55 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  generateParenthesis.cc
+ *
+ *    Description:  GG
+ *
+ *        Version:  1.0
+ *        Created:  05/23/16 05:46:49
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+    void generateParenthesis_(vector<string>& ret, string str, int l, int r, int n) {
+        if (0 == l && 0 == r) {
+            ret.push_back(str);
+	    return;
+	}
+	if (l > 0) {
+	    generateParenthesis_(ret, str + "(", l - 1, r, n);
+	}
+	if (r > 0 && r > l) {
+	    generateParenthesis_(ret, str + ")", l, r - 1, n);
+	}
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+	vector<string> ret;
+	string str = "";
+	generateParenthesis_(ret, str, n, n, n);
+	return ret;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Solution s;
+    vector<string> ret = s.generateParenthesis(2);
+    for(auto s : ret) {
+        cout << s << endl;
+    }
+    return 0;
+}
